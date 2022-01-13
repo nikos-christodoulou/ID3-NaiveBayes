11974d06d7a92f453408b4e4932508dae39ffb4c
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-13 11:37:16 +0200

    deleted weird comments

--- test.py
+++ test.py
@@ -4,18 +4,6 @@ from ID3 import DecisionTree
 import time
 
 import numpy as np 
-'''
-my_list = ([("the",0),("gay",1),("0",0),("a",1),("l",0),1],[("the",0),("gay",1),("0",0),("a",1),("l",0),0])
-this = list()
-
-
-for x in range(0,len(my_list)):
-    info_gain = [my_list[x][y][0] for y in range(0,len(my_list[x])-1)]
-print(info_gain)
-#print(sum(my_list[x][len(my_list[x])-1] for x in range(0,len(my_list))))
-#print(sum(1 for x in range (0,len(my_list)) if my_list[x][len(my_list[x])-1] == 0))
-#my_list[x][len(my_list[x])-1
-''' 
 # we use np array for handling two dimensional arrays better
 values_for_each_sentence = np.array(training_data_frame.drop('positive_or_negative',axis=1)).copy()
 values_for_positiveornegative = np.array(training_data_frame["positive_or_negative"]).copy()
