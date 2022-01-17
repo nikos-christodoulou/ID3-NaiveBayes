fe89731dcd118ea1862ed116c401ad168bd302d4
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-17 16:04:59 +0200

    commit

--- construct_training_examples.py
+++ construct_training_examples.py
@@ -1,7 +1,5 @@
-from numpy import positive
 from filter_the_vocabulary import vocabulary
-
-import os 
+import os,random,process_text,math
 import pandas as pd 
 folders = ['neg','pos']
 training_vector = dict()
@@ -10,35 +8,27 @@ We will create a binary vector with 1 if the word in the dictionary is present i
 else the value will be 0 if it's not contained in the word 
 this will be a pandas dataframe 
 ''' 
+percentage_of_data = math.ceil(12500) 
+per = True 
 training_data_frame = pd.DataFrame()
+
 for i in folders:
     path = "C:/Users/fotis/OneDrive/Desktop/Aiexercise2/aclImdb/train/" + i 
     os.chdir(path)
-    for file in os.listdir(): 
-        # check the text format 
-        if file.endswith(".txt"):
-            file_path = f"{path}/{file}"
-        f = open(file_path,'r',encoding="latin-1")
-        sentence = f.read()
-        #removing special characters so the vocabulary can represent tha data set better 
-        sentence = sentence.translate({ord(c): None for c in '().!@#$***'})
-        sentence = sentence.lower()
-        sentence = sentence.replace("<br />"," ")
-        splited_sentence = sentence.split() 
-        for key in vocabulary:
-            if(not(key in training_vector)):
-                training_vector[key] = list()
-            if(key in splited_sentence):
-                training_vector[key].append(1)
-            else:
-                training_vector[key].append(0)
-        if(not("positive_or_negative" in training_vector)):
-                training_vector["positive_or_negative"] = list()
-        if(i == "neg"):
-            training_vector["positive_or_negative"].append(0)
-        else:
-            training_vector["positive_or_negative"].append(1)
-        f.close()
+    if(per):
+        for y in range(0,percentage_of_data):
+            file = random.choice(os.listdir())
+            splited_sentence = process_text.split_sentence(file,path)
+            for key in vocabulary:
+                process_text.add_key(key,training_vector,splited_sentence)
+            process_text.add_target_val(training_vector,i)
+    else:
+        for file in os.listdir(): 
+            splited_sentence = process_text.split_sentence(file,path)
+            for key in vocabulary:
+                process_text.add_key(key,training_vector,splited_sentence)
+            process_text.add_target_val(training_vector,i)
+        
 
 training_data_frame = pd.DataFrame(training_vector)
 print(training_data_frame)
