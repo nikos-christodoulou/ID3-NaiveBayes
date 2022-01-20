71a1741d5dc5931b471eec1e214aaf42534657dd
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-20 02:09:27 +0200

    changed the path

--- construct_training_examples.py
+++ construct_training_examples.py
@@ -8,12 +8,12 @@ We will create a binary vector with 1 if the word in the dictionary is present i
 else the value will be 0 if it's not contained in the word 
 this will be a pandas dataframe 
 ''' 
-percentage_of_data = math.ceil(12500) 
+percentage_of_data = math.ceil(90/100 * 12500) 
 per = True 
 training_data_frame = pd.DataFrame()
 
 for i in folders:
-    path = "C:/Users/fotis/OneDrive/Desktop/Aiexercise2/aclImdb/train/" + i 
+    path = "C:/Users/fotis\OneDrive/Desktop/exer2AI/Aiexercise2/aclImdb/train/" + i
     os.chdir(path)
     if(per):
         for y in range(0,percentage_of_data):
