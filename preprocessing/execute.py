a78fe7216dc6cb529a0c7f77c2ccd0c0bc3ab3ec
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-21 19:14:54 +0200

    cbt cbt

--- preprocessing/execute.py
+++ preprocessing/execute.py
@@ -2,8 +2,11 @@ import subprocess
 import time
 b = [True,False]
 for z in b:
-    for x in range(10,110,10):
-        for y in range(10,110,10):
-            start = time.time()
-            subprocess.call("python C:/Users/fotis/OneDrive/Desktop/exer2AI/Aiexercise2/preprocessing/create_files.py " + str(x) + " " + str(z) + " " + str(y), shell=True)
-            print(time.time() - start)
+    for x in range(1,100):
+        if(x != 10 and x != 20 and x != 30 and x != 40 and x != 50 and x != 60 and x != 70 and x != 80 and x != 90):
+            for y in range(10,110,10):
+                start = time.time()
+                subprocess.call("python C:/Users/fotis/OneDrive/Desktop/exer2AI/Aiexercise2/preprocessing/create_files.py " + str(x) + " " + str(z) + " " + str(y), shell=True)
+                print(time.time() - start)
+        else:
+            continue
