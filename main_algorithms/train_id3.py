1f7a14483a7fafd280902ea03a52f1dcde413132
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-22 16:42:52 +0200

    read train files

--- main_algorithms/train_id3.py
+++ main_algorithms/train_id3.py
@@ -4,35 +4,18 @@ from ID3 import DecisionTree
 import numpy as np 
 import pandas as pd 
 from hyperparameters import per,approximate_logs,number_of_vocab_words
+from main_algorithms.read_train_files import create_vectors
+import read_train_files
 
 
-name =  "per_keys/" + str(int(per*100)) + "_" + str(number_of_vocab_words) + "_" + str(True) + ".txt"
-file = open(name,'r')
-cat_and_rev = file.readlines()
-categories = cat_and_rev[0].split()
-categories = categories[:-1]
-values_for_each_sentence = list()
-values_for_positiveornegative = list() 
-for x in range(1,len(cat_and_rev)):
-    values_for_positiveornegative.append(cat_and_rev[x][len(cat_and_rev[x]) - 2])
-    values_for_each_sentence.append(cat_and_rev[x][:-2].split())
-
-for x in values_for_each_sentence:
-    for y in range(len(x)):
-        x[y] = int(x[y])
-    
-values_for_each_sentence = np.array((values_for_each_sentence))
-
-
-for x in range(len(values_for_positiveornegative)):
-    values_for_positiveornegative[x] = int(values_for_positiveornegative[x])
-
-values_for_positiveornegative = np.array((values_for_positiveornegative))
-
-file.close()
+cat_and_rev = read_train_files(per,approximate_logs,number_of_vocab_words)
+t = create_vectors(cat_and_rev)
+categories = t[0]
+values_for_each_sentence = t[1]
+values_for_positiveornegative = t[2]
 
 p1 = DecisionTree(values_for_each_sentence,categories,values_for_positiveornegative)
 p1.ID3_start()
 if(p1.problem):
     p1.before_predict(p1.node)
-p1.print_tree(p1.node,p1.COUNT)
+#p1.print_tree(p1.node,p1.COUNT)
