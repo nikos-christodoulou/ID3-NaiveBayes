f36ceec805398f9aee3a1f72769b2d4e436bd4d4
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-01-27 05:22:12 +0200

    len 5 naive bayes

--- naive_bayes/hyperparameters.py
+++ naive_bayes/hyperparameters.py
@@ -5,8 +5,8 @@ Argument list should be
 Percentage of training data, approximate logs, number of vocab words, 
 '''
 
-if len(sys.argv) == 1: 
-    '''
+if len(sys.argv) == 5: 
+    
     per = float(sys.argv[1])/100
     approximate_logs = sys.argv[2] == "True"
     number_of_vocab_words = int(sys.argv[3])
@@ -16,7 +16,7 @@ if len(sys.argv) == 1:
     approximate_logs = False
     number_of_vocab_words = 10
     type_of_test = "test_examples"
-    
+    '''
     
 print("The percentage used will be: " + str(per))
 print("Will the file be the approximate log file: " + str(approximate_logs))
